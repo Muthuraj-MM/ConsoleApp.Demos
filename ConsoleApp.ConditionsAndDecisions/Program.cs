967b1dca0fb81ef43b9a4210c4948fbc2023a5a7
Muthuraj-MM <mmuthurajmariappan@gmail.com>
2025-08-16 07:06:54 +0530

    Enhanced ConditionsAndDecisions

--- ConsoleApp.ConditionsAndDecisions/Program.cs
+++ ConsoleApp.ConditionsAndDecisions/Program.cs
@@ -2,7 +2,7 @@
 int grade = Convert.ToInt32(Console.ReadLine());
 
 //Decision making using simple if-else statements
-if (grade >= 60)
+if (grade <= 100 && grade >= 35)
 {
     Console.WriteLine("You have passed the exam.");
 }
@@ -12,7 +12,11 @@ else
 }
 
 //Decision making using complex if-else statements
-if (grade >= 90)
+if ( grade < 0 || grade > 100)
+{
+    Console.WriteLine("Invalid grade value");
+}
+else if (grade >= 90)
 {
     Console.WriteLine("Grade: A");
 }
@@ -24,7 +28,7 @@ else if (grade >= 70)
 {
     Console.WriteLine("Grade: C");
 }
-else if (grade >= 60)
+else if (grade < 70 && grade >= 35)
 {
     Console.WriteLine("Grade: D");
 }
@@ -32,5 +36,36 @@ else
 {
     Console.WriteLine("Grade: F");
 }
+
+/*
+ * Ternary operator for grading
+*/
+
+string result = (grade >= 35 && grade <= 100) ? "Pass" : "Fail";
+Console.WriteLine($"Result using ternary operator: {result}");
+
+//Switch statement for grading
+Console.WriteLine("Switch Case Results");
+switch (grade)
+{
+    case >= 90:
+        Console.WriteLine("Grade: A");
+        break;
+    case >= 80:
+        Console.WriteLine("Grade: B");
+        break;
+    case >= 70:
+        Console.WriteLine("Grade: C");
+        break;
+    case >= 35:
+        Console.WriteLine("Grade: D");
+        break;
+    default:
+        Console.WriteLine("Grade: F");
+        break;
+}
+
 Console.WriteLine("Thank you for using the grading system!");
+
+
 
